003a9580022c3bd2eb74a4b7f30b4768b08742ce
alexandra brown & christopher calascione <student@epicodus.com>
2015-09-23 16:46:21 -0700

    add rating and approval for answers

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -1,14 +1,30 @@
 DoOrDont.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParams, AdviceFactory, UtilitiesFactory){
   $scope.question = UtilitiesFactory.findById(AdviceFactory.questions, $stateParams.questionId)
-
+  $scope.formShow = false;
+  $scope.rating;
   $scope.addAnswer = function() {
     if($scope.question.answers.indexOf($scope.answer) === -1) {
-      $scope.question.answers.push($scope.answer);
+      $scope.question.answers.push({text: $scope.answer,
+                                    id: $scope.question.answers.length + 1,
+                                    approved: false,
+                                    rating: 0,
+                                    upvote: 0});
       $scope.answer = null;
     } else {
       alert("answer already given");
     }
 
   };
+
+  $scope.addRating = function(id) {
+    $scope.question.answers[id-1].approved = true;
+    $scope.formShow = true;
+
+  }
+
+  $scope.submitRating = function(id, rating) {
+    $scope.formShow = false;
+    $scope.question.answers[id-1].rating = rating
+  }
 
 });
