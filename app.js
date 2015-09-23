58d1c98ed9a6c8ff2a00bb80f90260d8e1fdc54d
alexandra brown & christopher calascione <student@epicodus.com>
2015-09-23 11:49:43 -0700

    add first views

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-var courseRoster = angular.module('courseRoster', ['ui.router']);
+var DoOrDont = angular.module('DoOrDont', ['ui.router']);
 
-
-courseRoster.config(function($stateProvider, $urlRouterProvider){
+DoOrDont.config(function($stateProvider, $urlRouterProvider){
   $stateProvider.state('home', {
     url:'',
     templateUrl:'partials/home.html'
   });
 
-  $stateProvider.state('courses', {
-    url: '/courses',
-    templateUrl: 'partials/courses.html',
-    controller: 'CoursesCtrl'
+  $stateProvider.state('questions', {
+    url: '/questions',
+    templateUrl: 'partials/questions.html',
+    controller: 'QuestionsCtrl'
   });
 
-  $stateProvider.state('courses.students', {
-    url: "/:courseId",
-    templateUrl: 'partials/courses.students.html',
-    controller: 'StudentsCtrl'
-  });
+
 });
