5aaf1ea0d4db0c410d996097177f97f843a9d59f
calascionec <chris.calascione@gmail.com>
2015-12-03 23:04:38 -0800

    work on single question display and adding answers functionality

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -1,47 +1,12 @@
-DoOrDont.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParams, QuestionFactory, UtilitiesFactory, $firebaseObject, $firebaseArray){
-  // var ref = new Firebase("https://doordont.firebaseio.com/questions");
-  // $scope.question = UtilitiesFactory.findById(QuestionFactory.questions, $stateParams.questionId)
+DoOrDont.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParams, QuestionFactory, $firebaseObject, $firebaseArray){
+
   var ref = new Firebase("https://doordont.firebaseio.com/" + $stateParams.questionId);
   $scope.question = $firebaseObject(ref);
 
-  $scope.formShow = false;
-  $scope.rating;
-
   $scope.questionAnswers = $firebaseArray(ref.child("answers"));
 
-
   $scope.addAnswer = function() {
-    $scope.questionAnswers.$add({text: $scope.answer,
-                                      approved: false,
-                                      rating: 0,
-                                      upvote: 0});
+    $scope.questionAnswers.$add({text: $scope.answer});
     $scope.answer = '';
     };
-
-  $scope.addRating = function(id) {
-
-    var answer = $firebaseObject(ref.child('answers').child(id));
-    answer.$loaded(
-      function() {
-        answer.approved = true;
-        answer.$save();
-      }
-    )
-    $scope.formShow = true;
-  };
-
-    $scope.submitRating = function(id, rating) {
-      var answer = $firebaseObject(ref.child('answers').child(id));
-      answer.$loaded(
-        function() {
-          answer.rating = rating;
-          answer.$save();
-        }
-      )
-      $scope.formShow = false;
-  };
-
-
-
-
 });
